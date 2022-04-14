ce8d73e0d6598e4d1a15e070e3b87e64198b679b
Avinash20031999 <101059765+Avinash20031999@users.noreply.github.com>
2022-04-14 14:34:29 +0530

    Update Activity2.h

--- 3_Implementation/inc/Activity2.h
+++ 3_Implementation/inc/Activity2.h
@@ -1,1 +1,39 @@
+/**
+ * @file Activity2.h
+ * @author Avinash
+ * @Activity 2 header file
+ * @version 1.0
+ * @date 14th April 2022
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef __ACTIVITY_2_H__
+#define __ACTIVITY_2_H__
+/**
+ * @Including header files
+ * 
+ */
+ 
+#include <util/delay.h>
+#include <avr/io.h>
 
+/**
+ * @Intialising the function for ADC
+ * 
+ */
+void initialiseADC();
+
+/**
+ * 
+ * @returning the ADC value
+ */
+uint16_t readADC(uint8_t ch);
+
+/**
+ * @Return the ADC value to main function
+ * 
+ */
+uint16_t activity2(void);
+
+#endif
