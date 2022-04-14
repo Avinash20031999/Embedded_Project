93fa867d8502d63502785d0c8e6e90055bab59c5
Avinash20031999 <101059765+Avinash20031999@users.noreply.github.com>
2022-04-14 14:39:57 +0530

    Update user_utils.h

--- 3_Implementation/inc/user_utils.h
+++ 3_Implementation/inc/user_utils.h
@@ -1,1 +1,24 @@
+#ifndef __USER_UTILS_H__
+#define __USER_UTILS_H__
+/**
+ * @file user_utils.h
+ * @author Avinash
+ * @brief User defined utilities used across the project
+ * @version 1.0
+ * @date 14th April 2022
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "project_config.h"
+#include <util/delay.h>
 
+/**
+ * @brief Function to generate delay in micro seconds
+ * 
+ * @param[in] delay_time Delay in Micro seconds
+ * 
+ */
+void delay_ms(uint32_t delay_time);
+
+#endif /* __USER_UTILS_H__ */
