b6b6788c4a30a196dab92f1b26d7105a64cacda4
Avinash20031999 <101059765+Avinash20031999@users.noreply.github.com>
2022-04-14 14:48:56 +0530

    Update activity2.c

--- 3_Implementation/src/activity2.c
+++ 3_Implementation/src/activity2.c
@@ -1,1 +1,34 @@
+/**
+ * @file Activity2.c
+ * @author Avinash
+ * @ADC values 
+ * @version 1.0
+ * @date 12th April 2022
+ * 
+ * @copyright Copyright (c) 2022
+ */
+#include "activity2.h"
 
+void initialiseADC()
+{
+    ADMUX=(1<<REFS0);
+    ADCSRA= (1<<ADEN)|(7<<ADPS0);
+}
+uint16_t readadc(uint8_t ch)
+{
+    ADMUX&=0xf8;
+    ch=ch&0b00000111;
+    ADMUX|=ch;
+    ADCSRA|=(1<<ADSC);
+    while(!(ADCSRA & (1<<ADIF)));
+    ADCSRA|=(1<<ADIF);
+    return(ADC);
+}
+
+uint16_t activity2(void)
+{
+      initialiseADC();
+      uint16_t temp;
+      temp=readadc(0);
+      return temp;
+}
